372ad1da7c31593485dde8c6fe6f2cf3c6eec56e
Yang Yang <fisheepy@icloud.com>
2024-08-25 23:18:57 -0400

    Android usable

--- android/app/src/main/java/io/ionic/rtutapp/MainActivity.java
+++ android/app/src/main/java/io/ionic/rtutapp/MainActivity.java
@@ -1,5 +1,15 @@
 package io.ionic.rtutapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this);
+    }
+}
