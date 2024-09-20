40018747f88889d44f6aa7d2acde3634f7793d52
Yang Yang <fisheepy@icloud.com>
2024-09-19 22:35:17 -0400

    Event on each individual

--- src/calendarComponent.js
+++ src/calendarComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -6,6 +6,7 @@ import './App.css';
 import CustomEvent from './customEvent';
 import CustomAgendaEvent from './customAgendaEvent';
 import { Modal, StyleSheet, Text, View, ScrollView, TouchableWithoutFeedback } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const localizer = momentLocalizer(moment);
 
@@ -14,8 +15,27 @@ const CalendarComponent = ({ data }) => {
   const [view, setView] = useState(Views.MONTH);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [userFirstName, setUserFirstName] = useState(null);  // Store the user ID here
+  const [userLastName, setUserLastName] = useState(null);  // Store the user ID here
 
-  // Filter events based on the current view
+  // Fetch user ID from AsyncStorage on component mount
+  useEffect(() => {
+    const fetchUserId = async () => {
+      try {
+        const userFirstName = await AsyncStorage.getItem('userFirstName');
+        const userLastName = await AsyncStorage.getItem('userLastName');
+
+        setUserFirstName(userFirstName);
+        setUserLastName(userLastName);
+
+      } catch (error) {
+        console.error('Error fetching user ID:', error);
+      }
+    };
+    fetchUserId();
+  }, []);
+
+  // Filter events based on the current view and employees list
   const filteredData = useMemo(() => {
     let start, end;
 
@@ -26,14 +46,21 @@ const CalendarComponent = ({ data }) => {
       start = moment(currentDate).startOf(view).toDate();
       end = moment(currentDate).endOf(view).toDate();
     }
-
+    // Filter based on employees
     return data.filter(event => {
       const eventStart = new Date(event.startDate);
-      return eventStart >= start && eventStart <= end;
+      // If event has employees field, check if the user's ID is in employees.username
+      if (event.employees && event.employees.length > 0) {
+        return eventStart >= start && eventStart <= end && event.employees.some(emp => emp.firstName === userFirstName && emp.lastName === userLastName);
+      }
+      else{
+        return eventStart >= start && eventStart <= end;
+      }     
     });
-  }, [data, view, currentDate]);
+  }, [data, view, currentDate, userFirstName]);  // Ensure userId is included in dependencies
 
   const formattedData = useMemo(() => {
+    console.log(filteredData);
     return filteredData.map(event => ({
       ...event,
       start: new Date(event.startDate),
@@ -55,7 +82,6 @@ const CalendarComponent = ({ data }) => {
   const handleNavigate = (date) => {
     setCurrentDate(date);
 
-    // Adjust the date logic for agenda view to start from the next month if close to end
     if (view === Views.AGENDA) {
       const endOfMonth = moment(date).endOf('month');
       const daysLeft = endOfMonth.diff(date, 'days');
@@ -80,9 +106,9 @@ const CalendarComponent = ({ data }) => {
       <Calendar
         localizer={localizer}
         events={formattedData}
-        views={[Views.DAY, Views.MONTH, Views.AGENDA,]}
-        view={view} // Explicitly manage the view
-        date={currentDate} // Explicitly manage the date
+        views={[Views.DAY, Views.MONTH, Views.AGENDA]}
+        view={view}
+        date={currentDate}
         startAccessor="start"
         endAccessor="end"
         style={{ height: '95%', width: '95%' }}
@@ -99,8 +125,8 @@ const CalendarComponent = ({ data }) => {
         onView={handleViewChange}
         min={new Date(2024, 1, 1, 8, 0)}
         max={new Date(2024, 1, 1, 18, 0)}
-        step={30} // Number of minutes each slot represents
-        timeslots={2} // Number of slots per hour
+        step={30}
+        timeslots={2}
       />
       <Modal
         animationType="slide"
